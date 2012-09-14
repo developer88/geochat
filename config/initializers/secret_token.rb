a947c44e8af4c716715284e18fa6d2c55a9e7ffd
Eremin Andrey <dsoft88@gmail.com>
2012-09-14 21:44:13 +0400

    Added rails template project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geochat::Application.config.secret_token = '26df234edf8821f71ed20ff7b955e1faf8bf7c47e31b8ccd05614ba9fb233075836560139d4d5d736e0b085db95807a68136466ea7520baead2ed2f180bd6bb3'
