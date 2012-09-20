31506dc2dfa673e473b52b527ba348d68136f077
Eremin Andrey <dsoft88@gmail.com>
2012-09-20 23:24:18 +0400

    Adding loginza support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Geochat::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "log_out"
   get "login" => "sessions#new", :as => "log_in"
-  get "signup" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  post "authorise" => "sessions#create", :as => "authorise"
+  #get "signup" => "users#new", :as => "sign_up"
+  root :to => "sessions#new"
   resources :users
   resources :sessions
 end
